b8ad20f6a9a9e3a3ef7174264e773d6dc1250388
Khantuev Andrei <khantuevandrei@gmail.com>
2026-05-25 12:44:31 +0300

    Add handlers to complete and delete tasks in DOM

--- sprint-0/day-6-7/app.js
+++ sprint-0/day-6-7/app.js
@@ -13,8 +13,32 @@ function createTaskElement(task) {}
 function renderCounter() {}
 
 // Handlers
+
+// on Main
+document.querySelector(".main").addEventListener("click", (event) => {
+  handleTaskComplete(event);
+  handleTaskDelete(event);
+});
+
+// Complete task
+function handleTaskComplete(event) {
+  const completeBtn = event.target.closest("#card-complete");
+  if (!completeBtn) return;
+
+  const card = completeBtn.closest(".card");
+  card.classList.toggle("task-done");
+}
+// Delete task
+function handleTaskDelete(event) {
+  const deleteBtn = event.target.closest(".card-delete");
+  if (!deleteBtn) return;
+
+  const card = deleteBtn.closest(".card");
+  card.remove();
+}
+
 function handleAddTask(event) {}
-function handleTaskAction(event) {}
+
 function handleFilterChange(event) {}
 
 // Initialization
@@ -38,3 +62,16 @@ Task object structure:
   createdAt: "2024-01-15T10:30:00"
 }
 */
+
+// Overlay logic
+const overlay = document.querySelector(".modal-overlay");
+
+const openBtn = document.querySelector(".modal-open");
+openBtn.addEventListener("click", () => {
+  overlay.classList.remove("hidden");
+});
+
+const closeBtn = document.querySelector(".modal-close");
+closeBtn.addEventListener("click", () => {
+  overlay.classList.add("hidden");
+});
