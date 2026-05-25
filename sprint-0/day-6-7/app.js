7a791493d22dabc695588eca7d217ec9f247a700
Khantuev Andrei <khantuevandrei@gmail.com>
2026-05-25 07:50:52 +0300

    Add structure to app.js

---
+++ sprint-0/day-6-7/app.js
@@ -0,0 +1,27 @@
+// Data
+let tasks = [];
+let currentFilter = "all";
+
+// Utilities
+function geretateID() {}
+function saveToStorage() {}
+function loadFromStorage() {}
+
+// Rendering
+function renderTasks() {}
+function createTaskElement(task) {}
+function renderCounter() {}
+
+// Handlers
+function handleAddTask(event) {}
+function handleTaskAction(event) {}
+function handleFilterChange(event) {}
+
+// Initialization
+function init() {
+  loadFromStorage();
+  renderTasks();
+  // use handlers
+}
+
+document.addEventListener("DOMContentLoaded", init);
