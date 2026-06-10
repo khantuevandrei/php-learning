79841c6a227a1852825c141ca7a14f8bcf7511b0
Khantuev Andrei <khantuevandrei@gmail.com>
2026-06-10 20:59:30 +0500

    Copy basic project structure to build upon

---
+++ lesson-07/Database.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+class Database
+{
+    private static ?PDO $pdo = null;
+
+    public static function getInstance(): PDO
+    {
+        if (self::$pdo === null) {
+            $dsn = "pgsql:host=localhost;port=5432;dbname=sandbox";
+
+            self::$pdo = new PDO($dsn, 'myuser', 'mypass', [
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+                PDO::ATTR_EMULATE_PREPARES => false,
+            ]);
+        }
+
+        return self::$pdo;
+    }
+}
---
+++ lesson-07/UserController.php
@@ -0,0 +1,197 @@
+<?php
+
+declare(strict_types=1);
+
+class UserController
+{
+    private PDO $pdo;
+
+    public function __construct(PDO $pdo)
+    {
+        $this->pdo = $pdo;
+    }
+
+    // list of users
+    public function index()
+    {
+        $statement = $this->pdo->query("
+            SELECT * FROM users
+            ORDER BY id DESC
+        ");
+
+        $users = $statement->fetchAll();
+
+        require 'views/list.php';
+    }
+
+    // creation form
+    public function create()
+    {
+        $user = null;
+
+        require 'views/form.php';
+    }
+
+    // Validation
+    private function validate(array $data): array
+    {
+        $errors = [];
+
+        $name = $_POST['name'] ?? '';
+        $email = $_POST['email'] ?? '';
+        $age = $_POST['age'] ?? '';
+
+        if ($name === '') {
+            $errors['name'] = 'Name required';
+        }
+
+        if ($email === '') {
+            $errors['email'] = 'Email required';
+        } elseif (!str_contains($email, '@')) {
+            $errors['email'] = 'Incorrect email format';
+        }
+
+        if ($age === '') {
+            $errors['age'] = 'Age required';
+        } elseif ($age < 1 || $age > 120) {
+            $errors['age'] = 'Age must be between 1 and 120';
+        }
+
+        return $errors;
+    }
+
+    // Creating
+    public function store()
+    {
+        $name = $_POST['name'] ?? '';
+        $email = $_POST['email'] ?? '';
+        $age = $_POST['age'] ?? '';
+
+        $errors = $this->validate($_POST);
+
+        // If errors are present
+        if (!empty($errors)) {
+            $_SESSION['errors'] = $errors;
+
+            header("Location: index.php?action=create");
+            exit;
+        }
+
+        try {
+            $statement = $this->pdo->prepare("
+                INSERT INTO users (name, email, age)
+                VALUES (:name, :email, :age)
+            ");
+
+            $statement->execute([
+                ':name' => $name,
+                ':email' => $email,
+                ':age' => $age
+            ]);
+
+            $_SESSION['success'] = 'User created';
+
+            header("Location: index.php");
+            exit;
+        } catch (PDOException $e) {
+            error_log($e->getMessage());
+            header('Location: index.php');
+            exit;
+        }
+    }
+
+    // edit form
+    public function edit($id)
+    {
+        if ($id <= 0) die('Invalid ID');
+
+        try {
+            $statement = $this->pdo->prepare("
+            SELECT * FROM users
+            WHERE id = :id
+        ");
+
+            $statement->execute([':id' => $id]);
+
+            $user = $statement->fetch();
+
+            if (!$user) {
+                echo 'User not found';
+                exit;
+            }
+
+            require 'views/form.php';
+        } catch (PDOException $e) {
+            error_log($e->getMessage());
+            header('Location: index.php');
+            exit;
+        }
+    }
+
+    // editing
+    public function update($id)
+    {
+        if ($id <= 0) die('Invalid ID');
+
+        $name = $_POST['name'] ?? '';
+        $email = $_POST['email'] ?? '';
+        $age = $_POST['age'] ?? '';
+
+        $errors = $this->validate($_POST);
+
+        // If errors are present
+        if (!empty($errors)) {
+            $_SESSION['errors'] = $errors;
+
+            header("Location: index.php?action=edit&id=$id");
+            exit;
+        }
+
+        try {
+            $statement = $this->pdo->prepare("
+                UPDATE users
+                SET name = :name, email = :email, age = :age
+                WHERE id = :id
+            ");
+
+            $statement->execute([
+                ':name' => $name,
+                ':email' => $email,
+                ':age' => $age,
+                ':id' => $id
+            ]);
+
+            $_SESSION['success'] = 'User updated';
+
+            header("Location: index.php");
+            exit;
+        } catch (PDOException $e) {
+            error_log($e->getMessage());
+            header('Location: index.php');
+            exit;
+        }
+    }
+
+    // Deleting
+    public function delete($id)
+    {
+        if ($id <= 0) die('Invalid ID');
+
+        try {
+            $statement = $this->pdo->prepare("
+                DELETE FROM users
+                WHERE id = :id
+            ");
+
+            $statement->execute([':id' => $id]);
+
+            $_SESSION['success'] = 'User deleted';
+            header("Location: index.php");
+            exit;
+        } catch (PDOException $e) {
+            error_log($e->getMessage());
+            header('Location: index.php');
+            exit;
+        }
+    }
+}
