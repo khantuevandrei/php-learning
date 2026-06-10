d99708d80a667c1470913ed45051176b9eaee108
Khantuev Andrei <khantuevandrei@gmail.com>
2026-06-10 11:32:49 +0500

    Check for POST method. Save errors to an array. Show errors if any or complete message

--- lesson-03/handler.php
+++ lesson-03/handler.php
@@ -2,30 +2,42 @@
 
 declare(strict_types=1);
 
+if ($_SERVER['REQUEST METHOD'] !== 'POST') {
+    header('Location: form.php');
+    exit;
+}
+
 $name = $_POST['name'] ?? '';
 $email = $_POST['email'] ?? '';
 $age = $_POST['age'] ?? '';
+$age = (int) $age;
 
 $errors = [];
 
-if (empty($name)) {
-    echo 'Name cannot be empty';
+if ($name === '') {
+    $errors['name'] = 'Name cannot be empty';
 }
 
-if (empty($email)) {
-    echo 'Email cannot be empty';
+if ($email === '') {
+    $errors['email'] = 'Email cannot be empty';
+} elseif (!str_contains($email, '@')) {
+    $errors['email'] = 'Incorrect format';
 }
 
-if (str_contains($email, '@')) {
-    echo 'Incorrect format';
+if ($age === '') {
+    $errors['age'] = 'Age cannot be empty';
+} elseif ($age < 1 || $age > 120) {
+    $errors['age'] = 'Age must be between 1 and 120';
 }
 
-if (empty($age)) {
-    echo 'Age cannot be empty';
-}
+if (!empty($errors)) {
+    echo "<ul>";
 
-$age = (int) $age;
+    foreach ($errors as $error) {
+        echo "<li>" . htmlspecialchars($errror) . "</li>";
+    }
 
-if ($age < 1 || $age > 120) {
-    echo 'Age must be between 1 and 120';
+    echo "</ul>";
+} else {
+    echo "Thank you, " . htmlspecialchars($name) . " data recieved.";
 }
