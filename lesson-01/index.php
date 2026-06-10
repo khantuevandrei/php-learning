e0f0beba72536f1330286d4ebf3159b76a35cd09
Khantuev Andrei <khantuevandrei@gmail.com>
2026-06-10 08:35:10 +0500

    Add button to change request method

--- lesson-01/index.php
+++ lesson-01/index.php
@@ -2,6 +2,17 @@
 
 declare(strict_types=1);
 
-echo 'Server time: ' . date("Y-m-d") . ' ' . date('H:i:s') . "\n";
-echo 'Client IP: ' . $_SERVER['REMOTE_ADDR'] . "\n";
+echo 'Server time: ' . date("Y-m-d") . ' ' . date('H:i:s');
+echo 'Client IP: ' . $_SERVER['REMOTE_ADDR'];
 echo 'Request method: ' . $_SERVER['REQUEST_METHOD'];
+
+
+?>
+
+<?php if ($_SERVER['REQUEST_METHOD'] === 'POST'): ?>
+    <p>Это был POST-запрос.</p>
+<?php endif; ?>
+
+<form method="post">
+    <button type="submit">Отправить POST</button>
+</form>
